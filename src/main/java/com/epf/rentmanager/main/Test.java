4c8f2a1ebd3659bde3c2dcd566004e0c4b6f7867
Émile <emile.royer@epfedu.fr>
2023-03-10 16:33:50 +0100

    Rename Main.java to Test.java
    This better fits its purpose.

--- src/main/java/com/epf/rentmanager/main/Test.java
+++ src/main/java/com/epf/rentmanager/main/Test.java
@@ -4,7 +4,7 @@ import com.epf.rentmanager.exception.ServiceException;
 import com.epf.rentmanager.service.ClientService;
 import com.epf.rentmanager.service.VehicleService;
 
-public class Main {
+public class Test {
 	public static void main(String[] args) {
 		try {
 			System.out.println(ClientService.getInstance().findAll());
